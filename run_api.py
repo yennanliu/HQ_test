08be77d72a18469a9bcd0d551e857c5529d5e5b3
yennanliu <f339339@gmail.com>
2016-08-05 10:40:59 +0800

    Update run_api.py

--- run_api.py
+++ run_api.py
@@ -6,6 +6,17 @@ from flask import jsonify
 app = Flask(__name__)
 
 
+
+
+
+@app.route('/')
+def test():
+   return "<center><h1>WELCOME TO HQ API SERVICE ! <h1> <br> <h1> Please input URL as example below </h1></center>"  + \
+    " <center><br><h2>http://0.0.0.0:5000/hotelId=hotel_id&checkin_date=checkin_date&checkout_date=checkout_date</h2></center>" + \
+    "<center><br><h2>http://0.0.0.0:5000/hotelId=15&checkin_date=2012-11-19&checkout_date=2012-11-20</h2></center>"
+
+
+
 @app.route('/hotelId=<hotel_id>&checkin_date=<checkin_date>&checkout_date=<checkout_date>')
 def test2(hotel_id,checkin_date,checkout_date):
 	offer_data = API_run(hotel_id,checkin_date,checkout_date)
